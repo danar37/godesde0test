beb8e0c7be7a7b73bda36080d4b949cad1567f7e
unknown <dan.ar.ve.25@outlook.com>
2023-05-02 15:26:51 -0400

    Siguiente Commit

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	/*estado, texto := variables.ConviertoaTexto(2500)
 	fmt.Println(estado)
-	fmt.Println(texto)*/
+	fmt.Println(texto) */
 
 	if os := runtime.GOOS; os == "linux" || os == "OS X." {
 		fmt.Println("Esto no es Windows, es ", os)
